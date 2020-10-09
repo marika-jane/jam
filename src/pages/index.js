7c4831d172164a56d08c48d240cb987617fcc760
Marika Fiolitakis <marika.jf@gmail.com>
2020-10-09 17:55:06 +1300

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is my first change to the site.</p>
+    <p>Now I'm going to build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
